5bb01fe4be5f96be8baa3516b02dcf03f978f22d
saikumarponnaganti <97146447+saikumarponnaganti99@users.noreply.github.com>
2023-11-27 21:57:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from './components/Login'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+
+import JobItemDetails from './components/JobItemDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/jobs" component={Jobs} />
+        <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="not-found" />
+      </Switch>
+    )
+  }
 }
 
-export default App;
+export default App
